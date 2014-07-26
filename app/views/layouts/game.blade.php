efeff06fdbe733bf75d795630b54cc188f56226b
craig <craigzearfoss@yahoo.com>
2014-07-25 22:07:12 -0400

    made about and play pages

--- app/views/layouts/game.blade.php
+++ app/views/layouts/game.blade.php
@@ -8,13 +8,13 @@
     <!-- Viewport meta tag to prevent iPhone from scaling our page -->
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <link rel="stylesheet" href="./css/game.css"/>
+    <link rel="stylesheet" href="/css/game.css"/>
 
     <!-- Normalize hide address bar for iOS and Android -->
-    <script src="./js/hideaddressbar.js"></script>
+    <script src="/js/hideaddressbar.js"></script>
 
     <!-- Add media query support for IE8 and under. Must place media queries in external stylesheet -->
-    <script src="./js/respond.min.js"></script>
+    <script src="/js/respond.min.js"></script>
 
     <!--[if lt IE 9]>
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
